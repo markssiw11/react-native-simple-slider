21d90ca1eb08f41183437a1358e416b271856ab3
longnguyen <longnguyen95.xn@gmail.com>
2021-04-26 10:10:20 +0700

    remove component not use

--- Example/Slider.js
+++ Example/Slider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import SliderField from 'react-native-simple-slider';
 
 function Slider() {
   const [value, setValue] = React.useState();
